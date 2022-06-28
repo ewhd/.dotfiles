815892a45ae2747921941a15199226c4dce07bfa
ewhd <ewhd22@gmail.com>
2022-06-28 02:23:39 -0700

    'done' with vimrc and tmux.conf for now

---
+++ stow-script.zsh
@@ -0,0 +1,6 @@
+# This is an incomplete script to use stow in my dir setup
+
+# This is basically the format
+# This line as written will run stow in simulation mode, so nothing will be done
+# Mess around with the flags to do what you want
+stow --dotfiles -v -n -d /home/ewhd/.dotfiles/. -t /home/ewhd/ .

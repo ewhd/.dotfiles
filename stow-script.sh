d4d1f04291706f3272b70167a92a737779f3e0a6
ewhd <ewhd22@gmail.com>
2022-11-26 23:02:26 -0800

    made stow-script.sh useful

--- stow-script.sh
+++ stow-script.sh
@@ -3,4 +3,10 @@
 # This is basically the format
 # This line as written will run stow in simulation mode, so nothing will be done
 # Mess around with the flags to do what you want
-stow --dotfiles -v -n -d /home/ewhd/.dotfiles/. -t /home/ewhd/ .
+# stow --dotfiles -v -n -d /home/ewhd/.dotfiles/. -t /home/ewhd/ .
+
+# or just call these from ~/.dotfiles/
+stow --dotfiles stow/
+stow --dotfiles tmux/
+stow --dotfiles vim/
+stow --dotfiles zsh/
